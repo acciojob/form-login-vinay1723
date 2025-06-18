46e5a94e3ada58253bdd559eda374e2f6f18d46c
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-06-18 17:10:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+     let fname = document.querySelector('input[name="fname"]');
+     let lname = document.querySelector('input[name="lname"]');
 
+	let res = fname.value +" "+ lname.value;
+	 res = res.trim().split(" ").map((wrd) => wrd.replace(/[^a-z]/gi,"")).join(" ");
+	alert(res);
 }
